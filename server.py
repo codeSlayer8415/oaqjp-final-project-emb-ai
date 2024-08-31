0c9da5da28f8791348ee812cae79f5a6a4d941ff
Vrudhula Sreedhar <vrudhulasreedhar@gmail.com>
2024-08-31 10:04:45 -0400

    Added server.py file

---
+++ server.py
@@ -0,0 +1,25 @@
+'''
+Flask app that detects emotion of th input text provided in html template
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("This is an Emotion detection application")
+
+@app.route("/emotionDetector")
+def emotion():
+    text_to_analyze = request.args.get('textToAnalyze')
+    res = emotion_detector(text_to_analyze)
+    message = "For the given statement, the system response is "
+    for emotion, score in res.items():
+        message = message + emotion + ": " + str(score) + ", "
+    # in the end adding most dominant emotion
+    message = message + f"The most dominant emotion is {res['dominant_emotion']}"
+    return message
+
+@app.route("/")
+def get_html():
+    return render_template("index.html")
+# run the flask app
+app.run(port = 5000)
+

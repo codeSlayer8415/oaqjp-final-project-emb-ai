8a54c275c2aa7f8c12918672b7c874ca1c9485b6
Sreedhar Vrudhula <vrudhulasreedhar@gmail.com>
2024-09-01 03:56:41 -0400

    Made final changes related to static code anlysis

--- server.py
+++ server.py
@@ -7,14 +7,14 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("This is an Emotion detection application")
 
 @app.route("/emotionDetector")
-def emotion():
+def emotions():
+    '''Detects emotion of the input text and returns output according to it'''
     text_to_analyze = request.args.get('textToAnalyze')
     # if text_to_analyze == "":
     #     return "Input is blank! Try providing proper input"
     res = emotion_detector(text_to_analyze)
-    if res['dominant_emotion'] == None:
+    if res['dominant_emotion'] is None:
         return "Invalid text! Please try again"
-    # for 
     message = "For the given statement, the system response is "
     for emotion, score in res.items():
         message = message + emotion + ": " + str(score) + ", "
@@ -24,7 +24,8 @@ def emotion():
 
 @app.route("/")
 def get_html():
+    '''Returns basic html interface when app is opened(i.e. Get request)'''
     return render_template("index.html")
+
 # run the flask app
 app.run(port = 5000)
-
